24a09a3c54c71e0d27012685a4d8e19ac3e76b18
csomerville-yellow <charles.a.somerville@gmail.com>
2021-04-08 15:51:12 -0400

    fixed testing directory for API

--- backend/tests/tests_urls.py
+++ backend/tests/tests_urls.py
@@ -1,12 +1,17 @@
-from django.contrib.auth.models import AnonymousUser, User
 from django.test import TestCase, Client
+from django.urls import reverse, resolve
+from backend.api.models import models 
 
-class APITestCase(TestCase):
+class URLTestCase(TestCase):
+
     def setUp(self):
-    
         c = Client()
 
     def test_post(self):
         self.c.force_login()
         response = self.c.get('courses/1/')
         self.assertEqual(response.status_code, 200)
+
+    
+
+    
