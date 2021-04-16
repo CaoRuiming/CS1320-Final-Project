d42b878acfd090a8a11f01122e657078ba4f853f
csomerville-yellow <charles.a.somerville@gmail.com>
2021-04-15 22:41:23 -0400

    Charles final tests

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 from django.urls import reverse
-from api.views import UserView
 import json
 
 class BaseTest(TestCase):
@@ -8,33 +7,39 @@ class BaseTest(TestCase):
     def setUp(self):
         self.register_url = reverse('register')
         self.login_url = reverse('login')
-        self.invalid_credentials =  {'username' : 'gido', 'password' : '000000', 'email' : 'gido@theshore.org', 'first_name' : 'test', 'last_name' : 'user' }
+        self.invalid_credentials =  {'username' : 'testuser1', 'password' : '1234', 'email' : 'test@email.org', 'first_name' : 'test', 'last_name' : 'user' }
+        self.test_credentails =  {'username' : 'gido1', 'password' : '000000', 'email' : 'gido@theshore.org', 'first_name' : 'test', 'last_name' : 'user' }
         self.valid_credentials_ta= {
-            'username' : 'josiah_carberry@brown.edu' , 'password' : ''
+            'username' : 'josiah_carberry@brown.edu' , 'password' : 'mwjt73xw'
         }
         self.valid_credentials_student= {
-            'username' : 'josiah_carberry@brown.edu' , 'password' : ''
+            'username' : 'random@brown.edu' , 'password' : 'p1j448x8'
         }
+
+        self.client.post(self.register_url, json.dumps(self.test_credentails), content_type="application/json")
+        
         return super().setUp()
 
-class UserTest(BaseTest):
+class CreateUserTest(BaseTest):
     def test_register_user(self):
         
-        response=self.client.post(self.register_url, json.dumps(self.invalid_credentials), content_type="application/json")
+        registerCred =  {'username' : 'registered', 'password' : '000000', 'email' : 'fake@email.org', 'first_name' : 'test', 'last_name' : 'register' }
+        response=self.client.post(self.register_url, json.dumps(registerCred), content_type="application/json")
         self.assertEqual(response.status_code, 201)
-       # self.assertTemplateUsed(response, UserView.create_user )
+      
 
-   
-        # response=self.client.post(self.login_url, json.dumps(self.invalid_credentials), content_type="application/json" )
-        # self.assertEqual(response.status_code, 406)
-#test should fail for invalid login
 class LoginTest(BaseTest):
     def test_can_access_login(self):
-        response = self.client.get(self.login_url)
+        response = self.client.get(reverse('login'))
         self.assertEqual(response.status_code, 405)
+
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 201)
+
 
     def test_login_user(self):
         response=self.client.post(self.login_url, json.dumps(self.invalid_credentials), content_type="application/json" )
         self.assertEqual(response.status_code, 406)
 
-
+        response = self.client.post(self.login_url, json.dumps(self.test_credentails), content_type="application/json")
+        self.assertEqual(response.status_code, 201)
